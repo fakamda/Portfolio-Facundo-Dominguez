6112df3dd9a70358364724b62d441487c2dfa669
fakamda <facundoelcardenal@gmail.com>
2024-03-06 23:52:18 -0300

    a

---
+++ src/app/ui/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav>
+        <ul>
+            <li>About Me</li>
+            <li>Experience</li>
+            <li>Projects</li>
+            <li>Contact Me</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
