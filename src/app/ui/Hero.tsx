fc9b83d1213ee1b07e88ff8e6aa71f24773a749a
fakamda <facundoelcardenal@gmail.com>
2024-03-07 23:50:08 -0300

    glowing

--- src/app/ui/Hero.tsx
+++ src/app/ui/Hero.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image"
-import fondo from '../../assets/JAPAN4.jpg'
+import fondo from '../../assets/JAPAN5.png'
 import About from "./About"
 
 
@@ -12,7 +12,8 @@ const Hero = () => {
           <h3 className="text-4xl">Full Stack Developer</h3>
           <About />
         </div>
-        <Image className=" opacity-80" src={fondo} alt='fondo' />
+        <Image className=" opacity-90" src={fondo} alt='fondo' />
+        <div className="circlePosition w-[520px] h-[400px] bg-[#c56bb2] rounded-[100%] absolute z-[-1] top-[50%] left-[50%] translate-x-[9%] translate-y-[-50%] blur-[170px]"></div>
       </div>
     </div>
   )
