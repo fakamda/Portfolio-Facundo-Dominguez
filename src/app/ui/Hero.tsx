b7a9e676d7093cc386cb5156e83167de45daf2dc
fakamda <facundoelcardenal@gmail.com>
2024-03-13 23:31:21 -0300

    algo de responsive

--- src/app/ui/Hero.tsx
+++ src/app/ui/Hero.tsx
@@ -6,12 +6,12 @@ import About from "./About"
 const Hero = () => {
   return (
     <div className="mt-6">
-      <div className="flex flex-row justify-center items-center gap-6">
+      <div className="flex sm:flex-row flex-col justify-center items-center gap-6">
         <div className="flex flex-col">
         <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">Hola<span className="text-pink-400">.</span> Soy Facundo Dominguez</h2>
           <h3 className="text-4xl">Full Stack Developer</h3>
           {/* <div className="circlePosition w-full h-[200px] bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500  rounded-[100%] absolute z-[-1] top-[50%] left-[0%] translate-x-[40%] translate-y-[-200%] blur-[100px]"></div> */}
-          <div className="circlePosition w-[600px] h-[200px] bg-gradient-to-r from-orange-300 via-purple-400 to-pink-500 rounded-[100%] absolute z-[-1] blur-[140px]"></div>
+          <div className="circlePosition sm:w-[600px] w-[300px] h-[200px] bg-gradient-to-r from-orange-300 via-purple-400 to-pink-500 rounded-[100%] absolute z-[-1] blur-[140px]"></div>
           <About />
         </div>
         <Image className=" opacity-90" src={fondo} alt='fondo' />
--- src/app/ui/Tools.tsx
+++ src/app/ui/Tools.tsx
@@ -10,15 +10,15 @@ const Tools = () => {
         <h2 className='text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl'>
           Tools and Skills.
         </h2>
-        <div className='circlePosition w-[600px] h-[100px] bg-[#c56bb2] rounded-[100%] absolute z-[-1] translate-x-[35%] blur-[100px]'></div>
+        <div className='circlePosition sm:w-[600px] w-[300px] h-[100px] bg-[#c56bb2] rounded-[100%] absolute z-[-1] sm:translate-x-[35%] blur-[100px]'></div>
         <p className='mx-auto max-w-[600px] text-gray-500 md:text-xl/relaxed xl:text-base/relaxed dark:text-gray-400'>
           This are the main tools that I know and Use.
         </p>
       </div>
-      <div className="grid grid-cols-2 gap-10">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
         {
           cards.map((card, index) => (
-            <div key={index} className=" flex flex-col justify-center items-center gap-3 isolate aspect-video w-96 min-h-[410px] rounded-xl bg-white/20 shadow-lg ring-1 ring-black/5">
+            <div key={index} className=" flex flex-col justify-center items-center gap-3 isolate aspect-video sm:w-96 w-80 min-h-[410px] rounded-xl bg-white/20 shadow-lg ring-1 ring-black/5">
               <h2 className='text-2xl font-bold tracking-tighter sm:text-4xl md:text-2xl'>{card.title}</h2>
               <div className="flex flex-row flex-wrap justify-center items-center gap-3">
                 {card.payload}
