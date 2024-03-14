0bf1fd393e3ac4b9f4b13f98e8e5294d6466df7b
fakamda <facundoelcardenal@gmail.com>
2024-03-14 00:19:57 -0300

    nav

--- src/app/ui/Navbar.tsx
+++ src/app/ui/Navbar.tsx
@@ -1,5 +1,3 @@
-import logoImg from '../../assets/facundo.png'
-import Image from 'next/image'
 
 const Navbar = () => {
   return (
@@ -8,9 +6,9 @@ const Navbar = () => {
             Facudev<span className='text-pink-400'>.</span>
         </div>
         <ul className="flex flex-row justify-center items-center gap-8">
-            <li className="font-semibold text-white hover:text-pink-400 hover:bg-slate-800 rounded-md py-2 px-4">Experience</li>
-            <li className="font-semibold text-white hover:text-pink-400 hover:bg-slate-800 rounded-md py-2 px-4">Projects</li>
-            <li className="font-semibold text-white hover:text-pink-400 hover:bg-slate-800 rounded-md py-2 px-4">Contact Me</li>
+            <li className="font-semibold text-white hover:text-pink-400 rounded-md py-2 px-4">Experience</li>
+            <li className="font-semibold text-white hover:text-pink-400 rounded-md py-2 px-4">Projects</li>
+            <li className="font-semibold text-white hover:text-pink-400 rounded-md py-2 px-4">Contact Me</li>
         </ul>
     </nav>
   )
