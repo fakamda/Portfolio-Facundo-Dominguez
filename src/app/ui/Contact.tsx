2de8250671fc18a9f7c3478e4dd5a9ac49e5f54a
fakamda <facundoelcardenal@gmail.com>
2024-03-15 22:35:20 -0300

    a

--- src/app/ui/Contact.tsx
+++ src/app/ui/Contact.tsx
@@ -1,7 +1,22 @@
-
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className="flex flex-col justify-center items-center gap-5 m-10">
+      <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">Contact Me</h2>
+      <p className="mx-auto max-w-[600px] text-gray-500 md:text-xl/relaxed xl:text-base/relaxed dark:text-gray-400">
+        Leave your message and I will contact you back.
+      </p>
+      <div>
+      <form action="" className="flex flex-col justify-center items-center w-[400px]">
+        <label htmlFor="name" className="mx-auto max-w-[600px] text-gray-500 md:text-xl/relaxed xl:text-base/relaxed dark:text-gray-400">Your name</label>
+        <input type="text" name="name" id="" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" />
+        <label htmlFor="email" className="mx-auto max-w-[600px] text-gray-500 md:text-xl/relaxed xl:text-base/relaxed dark:text-gray-400">Email</label>
+        <input type="email" name="" id="" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"/>
+        <label htmlFor="message" className="mx-auto max-w-[600px] text-gray-500 md:text-xl/relaxed xl:text-base/relaxed dark:text-gray-400">Message</label>
+        <textarea name="message" id="" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light resize-none py-8"></textarea>
+      </form>
+      </div>
+      
+    </div>
   )
 }
 
