f07e19d58b1525ef3619af9af1e591bcc349e25e
fakamda <facundoelcardenal@gmail.com>
2024-03-26 22:01:03 -0300

    nodemailer

--- src/app/ui/Hero.tsx
+++ src/app/ui/Hero.tsx
@@ -10,7 +10,7 @@ const Hero = () => {
         <div className="flex flex-col">
         <h2 className="text-4xl font-bold tracking-tighter sm:text-4xl md:text-6xl">Hola<span className="text-pink-400">.</span></h2>
         <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl mt-1">Soy Facundo Dominguez</h2>
-          <h3 className="text-2xl">Full Stack Developer</h3>
+          <h3 className="text-2xl mt-2">Full Stack Developer</h3>
           {/* <div className="circlePosition w-full h-[200px] bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500  rounded-[100%] absolute z-[-1] top-[50%] left-[0%] translate-x-[40%] translate-y-[-200%] blur-[100px]"></div> */}
           <div className="circlePosition sm:w-[600px] w-[300px] h-[200px] bg-gradient-to-r from-orange-300 via-purple-400 to-pink-500 rounded-[100%] absolute z-[-1] blur-[140px]"></div>
           <About />
