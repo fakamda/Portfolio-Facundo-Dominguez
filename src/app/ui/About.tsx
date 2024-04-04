7bb19037f2e0a04f7129c99db0ec4155c4a029ed
fakamda <facundoelcardenal@gmail.com>
2024-04-04 01:00:22 -0300

    a

--- src/app/ui/About.tsx
+++ src/app/ui/About.tsx
@@ -11,8 +11,7 @@ const About = () => {
         <div className="flex flex-col justify-center gap-3 xl:w-[600px] md:w-[450px] w-full sm:h-[400px] text-wrap mt-4">
             <h3 className="text-lg font-bold text-pink-400">Sobre mi:</h3>
             <p>Soy un apasionado graduado en tecnología informática con experiencia como técnico en informática y actualmente estudiante de análisis en sistemas. Mi fascinación por el desarrollo fullstack se combina con un profundo interés en las telecomunicaciones, explorando cómo la tecnología conecta y transforma nuestro mundo. Fuera del mundo digital, disfruto sumergirme en juegos desafiantes y mantenerme en forma a través del entrenamiento físico. Busco constantemente nuevas oportunidades para expandir mis habilidades y contribuir al emocionante campo de la tecnología. </p>
-            {/* cargar cv y redes sociales */}
-            <div className="flex justify-start gap-4">
+            <div className="flex justify-start gap-4 mt-3">
               <Link href={"https://www.linkedin.com/in/facundo-dominguez-6557bb24b/"} target="_blank" className="flex justify-center items-center py-2 px-4 border-2 rounded-md gap-2">
                 LinkedIn
                 <FaLinkedin /> 
